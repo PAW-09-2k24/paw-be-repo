26d37d7bed13071f23dc901d986b5e62dbe7c6cc
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2024-09-15 12:32:32 +0700

    feat: add corsOption

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const handler404 = require('./middleware/404handler');
 const handler500 = require('./middleware/500handler');
+const cookieParser = require('cookie-parser');
 const app = express();
+const cors = require('cors');
+const {logger} = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Backend PAW Project 2024');
@@ -14,4 +24,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);   
 });
 
+app.use(errorHandler);
 
