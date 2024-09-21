c6d0a83a4efaecc9a98de5de5428c6c65ef59cbd
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2024-09-21 23:07:57 +0700

    feat: add verify middelware into roots

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {deleteGroup, createGroup, updateGroup} = require('../controller/groupController');
 const {getGroup} = require('../controller/taskController')
+const {verifyToken} = require('../middleware/jwtVerify');
 
 router.route('/')
-    .get(getGroup)
-    .post(createGroup)
-    .patch(updateGroup)
-    .delete(deleteGroup);
+    .get(verifyToken,getGroup)
+    .post(verifyToken,createGroup)
+    .patch(verifyToken,updateGroup)
+    .delete(verifyToken, deleteGroup);
 module.exports = router;
