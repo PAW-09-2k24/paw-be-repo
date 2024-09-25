80af851a92340f084c23f0ce73e99fd7384346e0
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2024-09-25 11:07:26 +0700

    fix: path cookies

--- controller/userController.js
+++ controller/userController.js
@@ -7,18 +7,18 @@ const jwt = require('jsonwebtoken');
 
 // @desc    Register a new user
 const registerUser = asyncHandler(async (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
     // confirm data
-    if(!username || !password){
-        return res.status(400).json({message: 'Please fill in all fields'});
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Please fill in all fields' });
     }
 
     // check if user exists
-    const duplicateUser = await User.findOne({username}).lean().exec();
+    const duplicateUser = await User.findOne({ username }).lean().exec();
 
-    if(duplicateUser){
-        return res.status(409).json({message: 'User already exists'});
+    if (duplicateUser) {
+        return res.status(409).json({ message: 'User already exists' });
     }
 
     //hash password
@@ -32,31 +32,31 @@ const registerUser = asyncHandler(async (req, res) => {
     // create and store user
     const user = await User.create(userObj);
 
-    if (user){
-        return res.status(201).json({message: 'User created successfully'});
+    if (user) {
+        return res.status(201).json({ message: 'User created successfully' });
     } else {
-        res.status(400),json({message: 'Invalid user data received'});
+        res.status(400), json({ message: 'Invalid user data received' });
     }
 
 });
 
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find({}).select('-password -__v').lean().exec();
-    if(users){
+    if (users) {
         return res.status(200).json(users);
     } else {
-        return res.status(404).json({message: 'No users found'});
+        return res.status(404).json({ message: 'No users found' });
     }
 });
 
 const login = asyncHandler(async (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    if(!username || !password){
-        return res.status(400).json({message: 'Please fill in all fields'});
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Please fill in all fields' });
     }
 
-    const user = await User.findOne({username}).lean().exec();
+    const user = await User.findOne({ username }).lean().exec();
 
     // if(!user){
     //     return res.status(404).json({message: 'User not found'});
@@ -64,29 +64,29 @@ const login = asyncHandler(async (req, res) => {
 
     const match = await bcrypt.compare(password, user.password);
 
-    if(match){
+    if (match) {
         const payload = {
             id: user._id,
             username: user.username
         }
-        const expiresIn = 60*60*1;
-        const token = jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: expiresIn});
-        res.cookie('token', token, {httpOnly: true, maxAge: expiresIn*1000, secure: true, sameSite: 'none'});
+        const expiresIn = 60 * 60 * 1;
+        const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: expiresIn });
+        res.cookie('token', token, { httpOnly: true, maxAge: expiresIn * 1000, secure: true, sameSite: 'none' , path: '/'});
         return res.status(200).json({
-            data:{
+            data: {
                 id: user._id,
                 username: user.username,
             },
-            token : token,
+            token: token,
             message: 'Login successful'
         });
     } else {
-        return res.status(401).json({message: 'Invalid credentials'});
+        return res.status(401).json({ message: 'Invalid credentials' });
     }
 });
 
-const logout = asyncHandler(async (req, res) => {});
+const logout = asyncHandler(async (req, res) => { });
 
 
 
-module.exports = {registerUser, getAllUsers, login, logout};
+module.exports = { registerUser, getAllUsers, login, logout };
